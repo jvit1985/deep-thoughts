1f322ff901f7101855bd435a97f19eee0f9f9ae4
Justin Vititoe <jvit1985@yahoo.com>
2022-07-12 21:16:27 -0500

    added middleware to connect react and node servers using concurrently

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -20,6 +21,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 //Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
